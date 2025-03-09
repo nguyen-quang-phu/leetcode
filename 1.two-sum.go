0c197b564e45afc9e05c65e3e320fb2229562d8d
keynold <nqphu1998@gmail.com>
2025-03-09 17:31:10 +0700

    feat: add first code

---
+++ 1.two-sum.go
@@ -0,0 +1,20 @@
+package leetcode
+
+// @leet start
+func twoSum(nums []int, target int) []int {
+	hash := make(map[int]int)
+
+	for i, num := range nums {
+		if _, ok := hash[target-num]; ok {
+			return []int{hash[target-num], i}
+		}
+
+		hash[num] = i
+	}
+
+	return nil
+}
+
+// @leet end
+
+// Keynold
